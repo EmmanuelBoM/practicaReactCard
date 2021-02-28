0372454a17277a5460d04ce3f0f20293358dedaa
Emmanuel Bolteada <a01276310@itesm.mx>
2021-02-28 17:33:59 -0600

    Fixed minor issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import Card from './card'
+import Page from './page'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let personalInfo = {
+    nombre: "Emmanuel",
+    apellidos: ['Bolteada', 'Manzo'],
+    correo: "a01276310@itesm.mx",
+    username: "EmmanuelBoM"
 }
 
-export default App;
+
+function App() {
+    return ( 
+        <div className = "App">
+            <header className = "App-header" >
+                <Router>
+                    <Switch>
+                        <Route 
+                        path = "/card"
+                        exact = { true }
+                        render = {(props) => <Card {...personalInfo } {...props }/> } / > { /* Aplicacion de Spread Syntax*/ }
+
+                        { /* Ruta dinámica */ } 
+                        <Route
+                            path = "/page/:section"
+                            exact = { true }
+                            render = {(props) => ( 
+                                <div> hello { props.match.params.section } </div>
+                            )}
+                        /> 
+                        <Route path = "/page/page1" exact = { true } > Page 1 </Route> / * Subpagina de / page * / 
+                        <Route path = "/page" > { Page } </Route> 
+                        <Route path = "/"exact = { true } >
+                            <React.Fragment>
+                                Welcome 
+                                <nav>
+                                    <Link to = "/card" > Card </Link> 
+                                    <Link to = "/page" > Page </Link> 
+                                </nav>
+                            </React.Fragment> 
+                        </Route> 
+                        <Route >
+                            Error 404. Page Not Found. 
+                        </Route> 
+                    </Switch> 
+                </Router> 
+            </header> 
+        </div>
+        );
+    }
+
+    export default App;
