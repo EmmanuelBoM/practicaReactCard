04c52e021be0179a116ada3bfc544c039bd0f376
Emmanuel Bolteada <a01276310@itesm.mx>
2021-02-28 18:09:29 -0600

    Practica card con syntactic sugar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import Card from './card'
 import Page from './page'
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 let personalInfo = {
@@ -11,56 +10,6 @@ let personalInfo = {
     apellidos: ['Bolteada', 'Manzo'],
     correo: "a01276310@itesm.mx",
     username: "EmmanuelBoM"
-=======
-import { BrowserRouter as Router, Route,Link, Switch} from 'react-router-dom'
-
-let personalInfo = {
-  nombre: "Emmanuel",
-  apellidos: ['Bolteada', 'Manzo'],
-  correo: "a01276310@itesm.mx",
-  username: "EmmanuelBoM"
-}
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <Switch>
-            <Route 
-            path="/card" 
-            exact={true}
-            render={(props) => <Card {...personalInfo}{...props}/> } /> {/* Aplicacion de Spread Syntax*/ }
-           
-            {/* Ruta dinámica */}
-            <Route
-              path="/page/:section" 
-              exact={true}
-              render = {(props)=>(
-                <div>hello {props.match.params.section}</div>
-              )}
-            />
-            <Route path="/page/page1" exact={true}>Page 1</Route> /* Subpagina de /page */
-            <Route path="/page">{Page}</Route>
-            <Route path="/" exact={true}>
-              <React.Fragment>
-                Welcome
-                <nav>
-                  <Link to="/card">Card</Link>
-                  <Link to="/page">Page</Link>
-                </nav>
-              </React.Fragment>
-            </Route>
-            <Route>
-              Error 404. Page Not Found.
-            </Route>
-          </Switch>
-        </Router>
-      </header>
-    </div>
-  );
->>>>>>> d80221068e913641e0741d4b8622c76602bc9431
 }
 
 
